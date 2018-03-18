7612c4508881b9cb63e92baa0b46cf60483a7779
Beatriz Palacios <bepa@unm.edu>
2018-03-18 17:35:39 -0600

    Use events from SRS diagram
    More issues with these when referencing the diagram. These are also a work in progress.

--- src/logic_layer/Event.java
+++ src/logic_layer/Event.java
@@ -1,9 +1,28 @@
 package logic_layer;
 
 /**
- *
+ * Based on the SRS control logic diagram
  */
 enum Event
 {
-    //
+    // Button events
+    BUTTON_PUSHED,
+    BUTTON_HELD,
+
+    // Brake events
+    BRAKE_ENGAGED_PARK,
+    BRAKE_DISENGAGED_PARK,
+    BRAKE_FULLY_ENGAGED,
+    BRAKE_FULLY_DISENGAGED,
+
+    // Speed events
+    SPEED_EQUAL_TO_ZERO,
+    SPEED_GREATER_THAN_ZERO,
+
+    // Transmission events
+    TRANSMISSION_SHIFT_IN_PARK,
+    TRANSMISSION_SHIFT_OUT_PARK,
+
+    // Timer events
+    TIMER_DISPATCH_FORCE, /* Open to suggestions on how to rephrase this. Event in the SRS diagram is confusing */
 }
