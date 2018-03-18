fab444f280511bc7557f6021bc9250d4c082366b
Beatriz Palacios <bepa@unm.edu>
2018-03-18 17:33:41 -0600

    Use actions from SRS diagram
    There are a few issues and inconsistencies with the diagram so this is still a work in progress. All of the old actions were kept for now. Only the sound actions must be refactored since the old brake actions complied with the SRS.

--- src/logic_layer/Action.java
+++ src/logic_layer/Action.java
@@ -1,12 +1,39 @@
 package logic_layer;
 
 /**
- *
+ * Based on the SRS control logic diagram
  */
 enum Action
 {
+    // Brake actions
     ENGAGE_BRAKE,
     DISENGAGE_BRAKE,
+    APPLY_BRAKE_FORCE,
+
+    // Light alert actions
+    SET_BLUE_LED,
+    SET_ORANGE_LED,
+    SET_RED_LED,
+
+    // Sound alert actions
+    /* Should we differentiate between continuous and single sound like this?
+       Diagram is very confusing
+     */
+    CONTINUOUS_ENGAGED_SOUND_ON,
+    SINGLE_ENGAGED_SOUND_ON,
+    SINGLE_DISENGAGED_SOUND_ON,
+    ENGAGED_SOUND_OFF,
+    /* This action isn't even in the diagram??? Even though there is an action that says
+       'Sound "electronic handbrake disengaged" alert'
+       And all the 'Sound "electronic handbrake engaged" alert' actions have a corresponding "stopping" action
+     */
+    DISENGAGED_SOUND_OFF,
+
+    // Old sound actions, will be discontinued by the ones above, whenever we understand that
     ENGAGE_SOUND,
-    DISENGAGE_SOUND
+    DISENGAGE_SOUND,
+
+    // "Nothing" action - used to represent the * where nothing happens
+    NON_ACTION,
+
 }
