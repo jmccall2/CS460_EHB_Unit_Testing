967ea3d94e9c27fda04570e08ee10d197fa6a4fb
Beatriz Palacios <bepa@unm.edu>
2018-03-18 17:31:47 -0600

    Use states from SRS diagram

--- src/logic_layer/State.java
+++ src/logic_layer/State.java
@@ -1,9 +1,21 @@
 package logic_layer;
 
 /**
- *
+ * Based on the SRS control logic diagram
  */
 enum State
 {
-    //
+    // Moving states
+    MOVING_ENGAGING,
+    MOVING_ENGAGED,
+    MOVING_DISENGAGING,
+    MOVING_DISENGAGED,
+
+    // Parked states
+    PARKED_ENGAGED,
+    PARKED_DISENGAGED,
+
+    // Stopped states
+    STOPPED_ENGAGED,
+    STOPPED_DISENGAGED,
 }
