110e34b99aeb3fe508f06f9770dfacbbd02cd2ed
Duong <dnguyen71@unm.edu>
2018-03-21 01:07:55 -0600

    logic_layer/Rules: whatActions method almost done, need to make clear about the starting events and actions (entry points) on parked Engaged and parked Disengaged

--- src/logic_layer/Rules.java
+++ src/logic_layer/Rules.java
@@ -1,7 +1,6 @@
 package logic_layer;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  *
@@ -13,9 +12,33 @@ public class Rules
      * @param currentState
      * @return
      */
-    private HashMap<Event,State> whatEvents(State currentState)
+    private static HashMap<Event,State> whatEvents(State currentState)
     {
-        //
+
+//        switch(currentState)
+//        {
+//            // Moving states
+//            case MOVING_ENGAGING:
+//                return null;
+//            case MOVING_ENGAGED:
+//                return null;
+//            case MOVING_DISENGAGING:
+//                return null;
+//            case MOVING_DISENGAGED:
+//                return null;
+//
+//            // Parked states
+//            case PARKED_ENGAGED:
+//                return null;
+//            case PARKED_DISENGAGED:
+//                return null;
+//
+//            // Stopped states
+//            case STOPPED_ENGAGED:
+//                return null;
+//            case STOPPED_DISENGAGED:
+//                return null;
+//        }
 
         return null;
     }
@@ -26,9 +49,74 @@ public class Rules
      * @param currentState
      * @return
      */
-    private List<Actions> whatActions(Event currentEvent, State currentState)
+    private static List<Action> whatActions(Event currentEvent, State currentState)
     {
-        //
+        switch(currentState)
+        {
+            // Moving states
+            case MOVING_ENGAGING:
+                if (currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.DISENGAGE_BRAKE, Action.SET_RED_LED);
+                else if(currentEvent == Event.BRAKE_FULLY_ENGAGED)
+                    return Arrays.asList(Action.SINGLE_ENGAGED_SOUND_ON, Action.SET_RED_LED);
+                else if(currentEvent == Event.TIMER_DISPATCH_FORCE)
+                    return Arrays.asList(Action.APPLY_BRAKE_FORCE);
+
+            case MOVING_ENGAGED:
+                if (currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.DISENGAGE_BRAKE,
+                                        Action.ENGAGED_SOUND_OFF);
+                else if(currentEvent == Event.SPEED_EQUAL_TO_ZERO)
+                    return Arrays.asList(Action.NON_ACTION);
+
+            case MOVING_DISENGAGING:
+                if (currentEvent == Event.BRAKE_FULLY_DISENGAGED)
+                    return Arrays.asList(Action.DISENGAGE_SOUND,
+                                        Action.SET_BLUE_LED);
+                else if(currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.SET_ORANGE_LED);
+
+            case MOVING_DISENGAGED:
+                if (currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.SET_ORANGE_LED);
+                else if(currentEvent == Event.SPEED_EQUAL_TO_ZERO)
+                    return Arrays.asList(Action.NON_ACTION);
+
+            // Parked states
+            case PARKED_ENGAGED:
+                if (currentEvent == Event.BUTTON_PUSHED)
+                    return Arrays.asList(Action.DISENGAGE_BRAKE,
+                                        Action.SET_BLUE_LED);
+                else if(currentEvent == Event.TRANSMISSION_SHIFT_OUT_PARK)
+                    return Arrays.asList(Action.CONTINUOUS_ENGAGED_SOUND_ON);
+            case PARKED_DISENGAGED:
+                if (currentEvent == Event.BUTTON_PUSHED)
+                    return Arrays.asList(Action.ENGAGE_BRAKE,
+                                        Action.SET_RED_LED);
+                else if(currentEvent == Event.TRANSMISSION_SHIFT_OUT_PARK)
+                    return Arrays.asList(Action.NON_ACTION);
+
+
+            // Stopped states
+            case STOPPED_ENGAGED:
+                if(currentEvent == Event.SPEED_GREATER_THAN_ZERO)
+                    return Arrays.asList(Action.NON_ACTION);
+                else if(currentEvent == Event.TRANSMISSION_SHIFT_IN_PARK)
+                    return Arrays.asList(Action.ENGAGED_SOUND_OFF);
+                else if(currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.DISENGAGE_BRAKE,
+                                        Action.ENGAGED_SOUND_OFF,
+                                        Action.SET_BLUE_LED);
+
+            case STOPPED_DISENGAGED:
+                if(currentEvent == Event.TRANSMISSION_SHIFT_IN_PARK)
+                    return Arrays.asList(Action.NON_ACTION);
+                else if(currentEvent == Event.BUTTON_HELD)
+                    return Arrays.asList(Action.ENGAGE_BRAKE,
+                                        Action.CONTINUOUS_ENGAGED_SOUND_ON,
+                                        Action.SET_RED_LED);
+
+        }
 
         return null;
     }
@@ -37,6 +125,6 @@ public class Rules
     /* For unit testing purposes */
     public static void main(String[] args)
     {
-        //
+
     }
 }
