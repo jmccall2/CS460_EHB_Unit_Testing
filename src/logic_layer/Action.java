bdec17dc3efcadd0b0d7b644634ab2c1c07586b3
Beatriz Palacios <bepa@unm.edu>
2018-03-23 11:35:19 -0600

    Revise cases for brake disengaged in action mapping
    Usage of low force versus high force were flipped. Might have to change the wording for these to avoid further confusion. Also, code clean up.

--- src/logic_layer/Action.java
+++ src/logic_layer/Action.java
@@ -36,32 +36,32 @@ enum Action
 
     /* The following actions will be discontinued after refactoring the logic interfaces */
     // Brake actions
-    ENGAGE_BRAKE,
-//    DISENGAGE_BRAKE,
-    APPLY_BRAKE_FORCE,
-
-    // old light alert
-    SET_ORANGE_LED,
-
-    // Sound alert actions
-    /* Should we differentiate between continuous and single sound like this?
-       Diagram is very confusing
-     */
-    CONTINUOUS_ENGAGED_SOUND_ON,
-    SINGLE_ENGAGED_SOUND_ON,
-    SINGLE_DISENGAGED_SOUND_ON,
-    ENGAGED_SOUND_OFF,
-    /* This action isn't even in the diagram??? Even though there is an action that says
-       'Sound "electronic handbrake disengaged" alert'
-       And all the 'Sound "electronic handbrake engaged" alert' actions have a corresponding "stopping" action
-     */
-    DISENGAGED_SOUND_OFF,
-
-    // Old sound actions, will be discontinued by the ones above, whenever we understand that
-    ENGAGE_SOUND,
-    DISENGAGE_SOUND,
-
-    // "Nothing" action - used to represent the * where nothing happens
-    NON_ACTION,
+//    ENGAGE_BRAKE,
+////    DISENGAGE_BRAKE,
+//    APPLY_BRAKE_FORCE,
+//
+//    // old light alert
+//    SET_ORANGE_LED,
+//
+//    // Sound alert actions
+//    /* Should we differentiate between continuous and single sound like this?
+//       Diagram is very confusing
+//     */
+//    CONTINUOUS_ENGAGED_SOUND_ON,
+//    SINGLE_ENGAGED_SOUND_ON,
+//    SINGLE_DISENGAGED_SOUND_ON,
+//    ENGAGED_SOUND_OFF,
+//    /* This action isn't even in the diagram??? Even though there is an action that says
+//       'Sound "electronic handbrake disengaged" alert'
+//       And all the 'Sound "electronic handbrake engaged" alert' actions have a corresponding "stopping" action
+//     */
+//    DISENGAGED_SOUND_OFF,
+//
+//    // Old sound actions, will be discontinued by the ones above, whenever we understand that
+//    ENGAGE_SOUND,
+//    DISENGAGE_SOUND,
+//
+//    // "Nothing" action - used to represent the * where nothing happens
+//    NON_ACTION,
 
 }
