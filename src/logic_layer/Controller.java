203f650b8c3246d21f1510d4f47ce1326db1d057
Beatriz Palacios <bepa@unm.edu>
2018-03-23 13:39:46 -0600

    Add comments with logic and behavior to implement for Controller
    Taken from the SAD from team 6.

--- src/logic_layer/Controller.java
+++ src/logic_layer/Controller.java
@@ -10,7 +10,18 @@ public class Controller
      */
     private void update()
     {
-        //
+        // TODO
+        // 1. Call whatEvents() from Rules with currentState
+        //    - use mapping from whatEvents() to know which events to "listen" to
+        //    - use mapping to know which State to move next
+        // 2. Iterate through the events and call didEventOccur() (from Events) on each event
+        //    - as long as we stay in the same state, keep calling didEventOccur() with the same list of events
+        //      for each update() call
+        // 3. When didEventOccur() returns true, call whatActions from Rules
+        //    - then we update the object's current State to the one that corresponds from he mapping in whatEvents()
+        //    - then iterate through all the actions returned by whatActions() and execute them
+        // 4. To execute each action, we must pass them to the execute() method in Actions
+        // 5. Repeat.
     }
 
     /* For unit testing purposes */
